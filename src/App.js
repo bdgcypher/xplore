ffa8e34875fbfdf74cb40890ed60e8a0674b95a9
Benjamin Goddard <bdgcypher@gmail.com>
2021-10-21 22:22:32 -0600

    trying to use hash router to fix routing issues.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import "tailwindcss/tailwind.css"
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Loader from './components/Loader/Loader';
 import Footer from './components/Footer';
